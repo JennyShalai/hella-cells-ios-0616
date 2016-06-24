d8c63517949a774b61b32a20262f9d802187e8d1
JennyShalai <jenny.shalai@gmail.com>
2016-06-24 11:03:28 -0400

    done

---
+++ FISHellaDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISHellaDetailViewController.h
+//  
+//
+//  Created by Flatiron School on 6/23/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaDetailViewController : UITableViewController
+
+@end
---
+++ HellaCells/FISHellaDetailViewController.m
@@ -0,0 +1,30 @@
+//
+//  FISHellaDetailViewController.m
+//  HellaCells
+//
+//  Created by Flatiron School on 6/24/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "FISHellaDetailViewController.h"
+
+@interface FISHellaDetailViewController ()
+
+@end
+
+@implementation FISHellaDetailViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    self.numberLabel.text = [NSString stringWithFormat:@"%lu", self.number];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
